888e22bf323272f8150142d77a8aac20e3f917e7
Inda Smith <inda.k.smith@gmail.com>
2023-06-06 19:30:10 -0400

    redid this lab but neater and needed review

--- index.js
+++ index.js
@@ -1,5 +1,47 @@
 // Write your code in this file!
-const currentUser = 'Katie Smith';
-const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + "!";
-const excitedWelcomeMessage = welcomeMessage.toUpperCase();
-const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
+
+const currentUser = "Katie Smith"
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase()
+
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const currentUser = 'Katie Smith';
+// const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + "!";
+// const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+// const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
